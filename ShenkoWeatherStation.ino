7322168dde29e9d62546ae51f73c664306ed3b58
Enos Shenk <enosshenk@gmail.com>
2019-06-01 19:37:07 -0400

    Create ShenkoWeatherStation.ino
    Arduino sketch commit

---
+++ ShenkoWeatherStation.ino
@@ -0,0 +1,403 @@
+/*
+  Shenko Heavy Weather
+  Sensor unit
+  by Enos Shenk
+
+  Built for Arduino Mega, Arduino Ethernet Shield, Sparkfun Weather Shield
+  
+  Some code from
+  SparkFun Electronics
+*/
+
+// Libraries for Sparkfun board and Ethernet Shield
+#include <Wire.h>
+#include <Ethernet.h>
+#include <SPI.h>
+
+// Adafruit BME280 pressure/temp/humidity sensor on a remote line
+#include <Adafruit_BME280.h>
+
+Adafruit_BME280 bme; // I2C
+
+//Hardware pin definitions
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// digital I/O pins
+const byte WSPEED = 3;
+const byte RAIN = 2;
+const byte STAT1 = 7;
+const byte STAT2 = 8;
+const byte HUM = 4;
+
+// analog I/O pins
+const byte REFERENCE_3V3 = A3;
+const byte WDIR = A0;
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//Global Variables
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+long lastSecond; //The millis counter to see when a second rolls by
+byte seconds; //When it hits 60, increase the current minute
+byte seconds_2m; //Keeps track of the "wind speed/dir avg" over last 2 minutes array of data
+byte minutes; //Keeps track of where we are in various arrays of data
+byte minutes_10m; //Keeps track of where we are in wind gust/dir over last 10 minutes array of data
+
+long lastWindCheck = 0;
+volatile long lastWindIRQ = 0;
+volatile byte windClicks = 0;
+
+byte windspdavg[120]; //120 bytes to keep track of 2 minute average
+
+#define WIND_DIR_AVG_SIZE 120
+int winddiravg[WIND_DIR_AVG_SIZE]; //120 ints to keep track of 2 minute average
+float windgust_10m[10]; //10 floats to keep track of 10 minute max
+int windgustdirection_10m[10]; //10 ints to keep track of 10 minute max
+volatile float rainHour[60]; //60 floating numbers to keep track of 60 minutes of rain
+
+//These are all the weather values that wunderground expects:
+// Only some used later
+int winddir = 0; // [0-360 instantaneous wind direction]
+float windspeedmph = 0; // [mph instantaneous wind speed]
+float windgustmph = 0; // [mph current wind gust, using software specific time period]
+int windgustdir = 0; // [0-360 using software specific time period]
+float windspdmph_avg2m = 0; // [mph 2 minute average wind speed mph]
+int winddir_avg2m = 0; // [0-360 2 minute average wind direction]
+float windgustmph_10m = 0; // [mph past 10 minutes wind gust mph ]
+int windgustdir_10m = 0; // [0-360 past 10 minutes wind gust direction]
+float humidity = 0; // [%]
+float tempf = 0; // [temperature F]
+float tempc = 0;
+float rainin = 0; // [rain inches over the past hour)] -- the accumulated rainfall in the past 60 min
+volatile float dailyrainin = 0; // [rain inches so far today in local time]
+//float baromin = 30.03;// [barom in] - It's hard to calculate baromin locally, do this in the agent
+float pressure = 0;
+float dewptf = 0; // [dewpoint F] - It's hard to calculate dewpoint locally, do this in the agent
+
+// volatiles are subject to modification by IRQs
+volatile unsigned long raintime, rainlast, raininterval, rain;
+
+
+// MAC address and IP for the Ethernet Shield, uses static IP on router
+byte mac[] = {
+  0xAA, 0xBB, 0xCC, 0xDD, 0x23, 0x35
+};
+IPAddress ip(192, 168, 1, 23);
+
+EthernetServer server(80);
+
+
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//Interrupt routines (these are called by the hardware interrupts, not by the main code)
+//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+void rainIRQ()
+// Count rain gauge bucket tips as they occur
+// Activated by the magnet and reed switch in the rain gauge, attached to input D2
+{
+  raintime = millis(); // grab current time
+  raininterval = raintime - rainlast; // calculate interval between this and last event
+
+  if (raininterval > 10) // ignore switch-bounce glitches less than 10mS after initial edge
+  {
+    dailyrainin += 0.011; //Each dump is 0.011" of water
+    rainHour[minutes] += 0.011; //Increase this minute's amount of rain
+
+    rainlast = raintime; // set up for next event
+  }
+}
+
+void wspeedIRQ()
+// Activated by the magnet in the anemometer (2 ticks per rotation), attached to input D3
+{
+  if (millis() - lastWindIRQ > 10) // Ignore switch-bounce glitches less than 10ms (142MPH max reading) after the reed switch closes
+  {
+    lastWindIRQ = millis(); //Grab the current time
+    windClicks++; //There is 1.492MPH for each click per second.
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(WSPEED, INPUT_PULLUP); // input from wind meters windspeed sensor
+  pinMode(RAIN, INPUT_PULLUP); // input from wind meters rain gauge sensor
+
+  pinMode(REFERENCE_3V3, INPUT);
+
+  // Start up BME280 sensor
+  bme.begin();  
+  
+  seconds = 0;
+  lastSecond = millis();
+
+  // attach external interrupt pins to IRQ functions
+  attachInterrupt(0, rainIRQ, FALLING);
+  attachInterrupt(1, wspeedIRQ, FALLING);
+
+  // turn on interrupts
+  interrupts();
+
+  // start the SPI library:
+  SPI.begin();
+
+  Ethernet.init(10);
+  // start the Ethernet connection
+  Ethernet.begin(mac, ip);
+
+    // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield not found");
+    while (true) {
+      delay(1);
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  server.begin();
+
+  // give the sensor and Ethernet shield time to set up:
+  delay(1000);
+}
+
+void loop()
+{
+  // All of the wind handling and averaging by Sparkfun
+  
+  //Keep track of which minute it is
+  if (millis() - lastSecond >= 1000)
+  {
+    lastSecond += 1000;
+
+    //Take a speed and direction reading every second for 2 minute average
+    if (++seconds_2m > 119) seconds_2m = 0;
+
+    //Calc the wind speed and direction every second for 120 second to get 2 minute average
+    float currentSpeed = get_wind_speed();
+    windspeedmph = currentSpeed;//update global variable for windspeed when using the printWeather() function
+    //float currentSpeed = random(5); //For testing
+    int currentDirection = get_wind_direction();
+    windspdavg[seconds_2m] = (int)currentSpeed;
+    winddiravg[seconds_2m] = currentDirection;
+    //if(seconds_2m % 10 == 0) displayArrays(); //For testing
+
+    //Check to see if this is a gust for the minute
+    if (currentSpeed > windgust_10m[minutes_10m])
+    {
+      windgust_10m[minutes_10m] = currentSpeed;
+      windgustdirection_10m[minutes_10m] = currentDirection;
+    }
+
+    //Check to see if this is a gust for the day
+    if (currentSpeed > windgustmph)
+    {
+      windgustmph = currentSpeed;
+      windgustdir = currentDirection;
+    }
+
+    if (++seconds > 59)
+    {
+      seconds = 0;
+
+      if (++minutes > 59) minutes = 0;
+      if (++minutes_10m > 9) minutes_10m = 0;
+
+      rainHour[minutes] = 0; //Zero out this minute's rainfall amount
+      windgust_10m[minutes_10m] = 0; //Zero out this minute's gust
+    }
+  }
+  
+  // listen for incoming Ethernet connections:
+  listenForEthernetClients();
+
+  calcWeather();
+  delay(100);
+}
+
+void listenForEthernetClients() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("Got a client");
+    // an http request ends with a blank line
+    bool currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println();
+          // print the current readings seperated by breaks. This will be handled later by Java to send to WUnderground
+          client.print(tempf);
+          client.print("<br />");
+          client.print(humidity);
+          client.print("<br />");
+          client.print(windspeedmph);
+          client.print("<br />");
+          client.print(winddir_avg2m);
+          client.print("<br />");
+          client.print(windgustmph_10m);
+          client.print("<br />");
+          client.print(rainin);
+          client.print("<br />");
+          client.print(dailyrainin);
+          client.print("<br />");  
+          client.print(pressure);
+          client.print("<br />");
+          client.print(dewptf);
+          client.print("<br />");                     
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+  }
+}
+
+//Calculates each of the variables that wunderground is expecting
+void calcWeather()
+{
+  //Calc winddir
+  winddir = get_wind_direction();
+
+  //Calc windspdmph_avg2m
+  float temp = 0;
+  for (int i = 0 ; i < 120 ; i++)
+    temp += windspdavg[i];
+  temp /= 120.0;
+  windspdmph_avg2m = temp;
+
+  //Calc winddir_avg2m, Wind Direction
+  //You can't just take the average. Google "mean of circular quantities" for more info
+  //We will use the Mitsuta method because it doesn't require trig functions
+  //And because it sounds cool.
+  //Based on: http://abelian.org/vlf/bearings.html
+  //Based on: http://stackoverflow.com/questions/1813483/averaging-angles-again
+  long sum = winddiravg[0];
+  int D = winddiravg[0];
+  for (int i = 1 ; i < WIND_DIR_AVG_SIZE ; i++)
+  {
+    int delta = winddiravg[i] - D;
+
+    if (delta < -180)
+      D += delta + 360;
+    else if (delta > 180)
+      D += delta - 360;
+    else
+      D += delta;
+
+    sum += D;
+  }
+  winddir_avg2m = sum / WIND_DIR_AVG_SIZE;
+  if (winddir_avg2m >= 360) winddir_avg2m -= 360;
+  if (winddir_avg2m < 0) winddir_avg2m += 360;
+
+  //Find the largest windgust in the last 10 minutes
+  windgustmph_10m = 0;
+  windgustdir_10m = 0;
+  //Step through the 10 minutes
+  for (int i = 0; i < 10 ; i++)
+  {
+    if (windgust_10m[i] > windgustmph_10m)
+    {
+      windgustmph_10m = windgust_10m[i];
+      windgustdir_10m = windgustdirection_10m[i];
+    }
+  }
+
+  //Get readings from the BME sensor
+  
+  // Humidity
+  humidity = bme.readHumidity();
+   //pressure
+  pressure = bme.readPressure();
+
+  // Temperture reports in Celsius, convert it to FREEDOM UNITS
+  tempc = bme.readTemperature();
+  tempf = (tempc * 1.8) + 32;
+
+  // Normalize temperture
+  // This is hacky as hell to try to mitigate sun directly on the sensor housing
+  if (tempf > 64)
+  {
+    tempf = tempf - (( tempf - 70) / 1.9);
+  }
+
+  // Only try to do dew point calculations if we have valid temp and humidity readings
+  // Otherwise we may divide by zero
+  float dewptc = 0;
+  if (tempc != 0 && humidity != 0)
+  {
+    // Calculate dew point
+    float k;
+    k = log(humidity / 100) + (17.62 * tempc) / (243 + tempc);
+    dewptc = 243.12 * k / (17.62 - k);
+    dewptf = (dewptc * 1.8) + 32;
+  }
+
+  //Total rainfall for the day is calculated within the interrupt
+  //Calculate amount of rainfall for the last 60 minutes
+  rainin = 0;
+  for (int i = 0 ; i < 60 ; i++)
+    rainin += rainHour[i];
+}
+
+//Returns the instataneous wind speed
+float get_wind_speed()
+{
+  float deltaTime = millis() - lastWindCheck; //750ms
+
+  deltaTime /= 1000.0; //Covert to seconds
+
+  float windSpeed = (float)windClicks / deltaTime; //3 / 0.750s = 4
+
+  windClicks = 0; //Reset and start watching for new wind
+  lastWindCheck = millis();
+
+  windSpeed *= 1.492; //4 * 1.492 = 5.968MPH
+
+  return (windSpeed);
+}
+
+//Read the wind direction sensor, return heading in degrees
+int get_wind_direction()
+{
+  unsigned int adc;
+
+  adc = analogRead(WDIR); // get the current reading from the sensor
+
+  // The following table is ADC readings for the wind direction sensor output, sorted from low to high.
+  // Each threshold is the midpoint between adjacent headings. The output is degrees for that ADC reading.
+  // Note that these are not in compass degree order! See Weather Meters datasheet for more information.
+
+  if (adc < 380) return (113);
+  if (adc < 393) return (68);
+  if (adc < 414) return (90);
+  if (adc < 456) return (158);
+  if (adc < 508) return (135);
+  if (adc < 551) return (203);
+  if (adc < 615) return (180);
+  if (adc < 680) return (23);
+  if (adc < 746) return (45);
+  if (adc < 801) return (248);
+  if (adc < 833) return (225);
+  if (adc < 878) return (338);
+  if (adc < 913) return (0);
+  if (adc < 940) return (293);
+  if (adc < 967) return (315);
+  if (adc < 990) return (270);
+  return (-1); // error, disconnected?
+}
